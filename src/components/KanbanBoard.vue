06b1994a4547ef5b300298ac5c548b8f517d395b
thanh <thanh.nguyen@tenpoint7.com>
2022-07-13 10:14:07 +0700

    Update Drag drop

--- src/components/KanbanBoard.vue
+++ src/components/KanbanBoard.vue
@@ -1,47 +1,138 @@
 <template>
-    <div v-if="group" class="h-full bg-slate-800 flex flex-col">
-        <div class="w-full h-16 flex-none flex bg-slate-700 items-center p-2">
-            <div  class="font-semibold text-bg flex-auto text-white">
-                <input type="text" class="bg-transparent rounded-lg" :value="group.name" @change="store.commit('editGroup', {
-                    id: group?.id,
-                    name: $event.target?.value
-                })">
-            </div>
+  <div v-if="group" class="h-full bg-slate-800 flex flex-col">
+    <div class="w-full h-16 flex-none flex bg-slate-700 items-center p-2">
+      <div class="font-semibold text-bg flex-auto text-white">
+        <input
+          type="text"
+          class="bg-transparent rounded-lg"
+          :value="group.name"
+          @change="
+            store.commit('editGroup', {
+              id: group?.id,
+              name: $event.target?.value,
+            })
+          "
+        />
+      </div>
+    </div>
+    <div class="flex flex-nowrap overflow-x-auto flex-auto">
+      <div
+        class="flex flex-col w-64 ml-6"
+        :key="item.id"
+        v-for="item in tickets"
+        :name="item.name"
+      >
+        <div
+          class="text-white text-sm text-center justify-center items-center uppercase p-3 font-semibold flex"
+        >
+          <span class="text-bg">•</span>
+          <input
+            @change="
+              store.commit('editTicket', {
+                ...item,
+                name: $event.target.value,
+              })
+            "
+            class="bg-transparent border-none active:border-none"
+            type="text"
+            :value="item.name"
+          />
         </div>
-        <div class="flex flex-nowrap overflow-x-auto flex-auto">
-            <tasks-col @selectTask="selectedTask = $event" :tasks="item.tasks" @changeTask="store.commit('editTask', $event)" @setTask="store.commit('addTask', item.id)" @setName="store.commit('editTicket', {
+        <div class="flex-auto flex flex-col rounded-lg bg-slate-700">
+          <draggable v-model="item.tasks" item-key="id" group="people" @change="change($event)">
+            <template #item="task">
+              <div
+                class="m-2 w-auto rounded-lg bg-slate-800 h-32 border-4 flex flex-col text-white p-2 border-none justify-center"
+              >
+                <input
+                  @change="
+                    store.commit('editTask', {
+                      ...task.element,
+                      name: $event.target.value,
+                    })
+                  "
+                  :value="task.element.name"
+                  class="bg-transparent h-8 font-semibold rounded-lg focus-visible:border-none"
+                  id=""
+                />
+                <div
+                  @click="selectedTask = task.element"
+                  class="flex-auto text-sm text-gray-500 font-semibold"
+                >
+                  {{ task.element.subTasks.filter((s) => s.checked).length }}
+                  of {{ task.element.subTasks.length }} subtasks
+                </div>
+              </div>
+            </template>
+          </draggable>
+          <!-- <div class="" v-for="task in props.tasks" :key="task.id">
+                        
+                    </div> -->
+          <button
+            @click="store.commit('addTask', item.id)"
+            class="m-2 border-dashed w-auto rounded-lg border-slate-800 h-32 border-4 flex justify-center items-center text-9xl text-slate-800 hover:border-slate-900 hover:text-slate-900"
+          >
+            +
+          </button>
+        </div>
+      </div>
+      <!-- <tasks-col @selectTask="selectedTask = $event" :tasks="item.tasks" @changeTask="store.commit('editTask', $event)" @setTask="store.commit('addTask', item.id)" @setName="store.commit('editTicket', {
                 ...item,
                 name: $event
-            })" :key="item.id" v-for="item in tickets" :name="item.name"  class="w-64 ml-6" color="text-blue-500"></tasks-col>
-            <button @click="store.commit('addTicket', group?.id)" class="border-dashed w-64 m-2 mt-6 rounded-lg border-slate-700 h-full border-4 flex justify-center items-center text-9xl text-slate-700 hover:border-slate-900 hover:text-slate-900">+</button>
-        </div>
+            })" :key="item.id" v-for="item in tickets" :name="item.name"  class="w-64 ml-6" color="text-blue-500"></tasks-col> -->
+      <button
+        @click="store.commit('addTicket', group?.id)"
+        class="border-dashed w-64 m-2 mt-6 rounded-lg border-slate-700 h-full border-4 flex justify-center items-center text-9xl text-slate-700 hover:border-slate-900 hover:text-slate-900"
+      >
+        +
+      </button>
     </div>
-    <task-dialog @close="selectedTask = null" :groupTask="tickets" :task="selectedTask"></task-dialog>
-
+  </div>
+  <task-dialog
+    @close="selectedTask = null"
+    :groupTask="tickets"
+    :task="selectedTask"
+  ></task-dialog>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { useRoute } from 'vue-router';
-import { useStore } from 'vuex';
-import { KanbanTask, State,  } from '../main';
+import { computed, ref } from "vue";
+import { useRoute } from "vue-router";
+import { useStore } from "vuex";
+import { KanbanTask, State } from "../main";
+import draggable from "vuedraggable";
 
-import TasksCol from './TasksCol.vue';
-import TaskDialog from './TaskDialog.vue';
+import TasksCol from "./TasksCol.vue";
+import TaskDialog from "./TaskDialog.vue";
 
 const store = useStore<State>();
 const route = useRoute();
 const groups = computed(() => store.state.groups);
 
-const group = computed(() => groups.value.find(g => g.id === parseInt(route.params.id.toString())));
-const tickets = computed(() => store.state.tickets.filter(ticket => ticket.groupId === group.value.id).map(ticket => ({
-    ...ticket,
-    tasks: store.state.tasks.filter(task => task.groupId === ticket.id)
-})));
-const selectedTask = ref<KanbanTask>(null);
+const group = computed(() =>
+  groups.value.find((g) => g.id === parseInt(route.params.id.toString()))
+);
+const tickets = computed(() =>
+  store.state.tickets
+    .filter((ticket) => ticket.groupId === group.value.id)
+    .map((ticket) => ({
+      ...ticket,
+      tasks: store.state.tasks.filter((task) => task.groupId === ticket.id),
+    }))
+);
+function change(params: { added: { element: any; newIndex: number;} }) {
+    console.log(params);
+    
+    if (params.added && params.added.element) {
+        
+        store.commit('editTask', {
+            ...params.added.element,
+            groupId: tickets.value[params.added.newIndex].id
+        })
 
+    }
+}
+const selectedTask = ref<KanbanTask>(null);
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/TasksCol.vue
+++ src/components/TasksCol.vue
@@ -4,13 +4,20 @@
             <span class="text-bg" :class="props.color">•</span> <input @change="emits('setName', $event.target['value'])" class="bg-transparent border-none active:border-none" type="text" :value="props.name">
         </div>
         <div  class="flex-auto flex flex-col rounded-lg bg-slate-700">
-            <div class="m-2 w-auto rounded-lg bg-slate-800 h-32 border-4 flex flex-col text-white p-2 border-none justify-center" v-for="task in props.tasks" :key="task.id">
-                <input @change="emits('changeTask', {
-                    ...task,
-                    name: $event.target.value
-                })" :value="task.name" class="bg-transparent h-8 font-semibold rounded-lg focus-visible:border-none" id=""  />
-                <div @click="emits('selectTask', task)" class="flex-auto text-sm text-gray-500 font-semibold">{{task.subTasks.filter(s => s.checked).length}} of {{task.subTasks.length}} subtasks </div>
-            </div>
+            <draggable v-model="tasks" item-key="id">
+                <template #item="task">
+                    <div class="m-2 w-auto rounded-lg bg-slate-800 h-32 border-4 flex flex-col text-white p-2 border-none justify-center" >
+                        <input @change="emits('changeTask', {
+                            ...task.element,
+                            name: $event.target.value
+                        })" :value="task.element.name" class="bg-transparent h-8 font-semibold rounded-lg focus-visible:border-none" id=""  />
+                        <div @click="emits('selectTask', task)" class="flex-auto text-sm text-gray-500 font-semibold">{{task.element.subTasks.filter(s => s.checked).length}} of {{task.element.subTasks.length}} subtasks </div>
+                    </div>
+                </template>
+            </draggable>
+            <!-- <div class="" v-for="task in props.tasks" :key="task.id">
+                
+            </div> -->
             <button @click="emits('setTask')" class="m-2 border-dashed w-auto rounded-lg border-slate-800 h-32 border-4 flex justify-center items-center text-9xl text-slate-800 hover:border-slate-900 hover:text-slate-900">+</button>
 
         </div>
@@ -19,6 +26,8 @@
 
 <script setup lang="ts">
 import { KanbanTask } from "../main";
+import draggable from "vuedraggable";
+import { computed, ref, watch } from "@vue/runtime-core";
 
 
 const props = defineProps<{
@@ -26,7 +35,12 @@ const props = defineProps<{
   color: string;
   tasks: KanbanTask[]
 }>();
-const emits = defineEmits(['setName', 'setTask', 'changeTask', 'selectTask'])
+
+const tasks = ref([]);
+const emits = defineEmits(['setName', 'setTask', 'changeTask', 'selectTask']);
+watch(() => props.tasks, (_tasks) => {
+    tasks.value = _tasks;
+})
 
 
 </script>
