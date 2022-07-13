ca5f265fddb3fe31e2e3e15639756045769be4e5
thanh <thanh.nguyen@tenpoint7.com>
2022-07-13 16:34:07 +0700

    Init Dashboard

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './index.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-createApp(App).mount('#app')
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { createRouter, createWebHistory } from 'vue-router';
+import Dashboard from './views/Dashboard.vue'
+
+const routes = [
+    {
+      path: "/admin/dashboard",
+      component: Dashboard,
+    }
+  ];
+  
+  const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
+  
+
+library.add(faHatWizard);
+
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+createApp(App).use(router).mount('#app')
